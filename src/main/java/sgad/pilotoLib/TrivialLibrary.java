7a5ec4a08f3b0df159fc966e458dd738bc420fc6
VICTOR PABLOS CERUELO <victor.pablos@externos.correo.gob.es>
2020-06-09 16:30:16 +0200

    Trivial library done.

---
+++ src/main/java/sgad/pilotoLib/TrivialLibrary.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgad.pilotoLib;
+
+/**
+ *
+ * @author me
+ */
+public class TrivialLibrary {
+    
+    public TrivialLibrary() {
+        
+    }
+    
+    public String getMessage() {
+        return "Hello World by the library ! ";
+    }
+    
+}
